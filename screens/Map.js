3216bc85dfb5fe9a3c76bbe42f3145e284576d1f
Nhan Phan <kippulainenko@gmail.com>
2022-09-10 18:50:41 +0300

    use MapView and Marker in location picker

--- screens/Map.js
+++ screens/Map.js
@@ -1,19 +1,39 @@
+import { useState } from "react";
 import { StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps"
 
 const Map = () => {
-
+    const [selectedLocation, setSeletecedLocation] = useState()
     const region = {
         latitude: 37.78,
         longitude: -122,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421
     };
+    function selectLocationHanlder(event) {
+        console.log(event)
+        const lat = event.nativeEvent.coordinate.latitude;
+        const lng = event.nativeEvent.coordinate.longitude;
+
+        setSeletecedLocation({
+            lat: lat,
+            lng: lng
+        })
+    }
 
   return (
     <MapView 
     style={styles.map}
-    initialRegion={region}></MapView>
+    initialRegion={region}
+    onPress={selectLocationHanlder}    
+    >
+        {selectedLocation && (
+            <Marker
+            title='Picked Location'
+            coordinate={{latitude: selectedLocation.lat, longitude: selectedLocation.lng}}
+            />
+        )}
+    </MapView>
   )
 }
 
